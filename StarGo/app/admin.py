00ee57f548471e85e272e9ec2792c5fd9aa78997
nuannim <66070286@kmitl.ac.th>
2025-10-04 18:10:53 +0700

    Update admin.py

--- StarGo/app/admin.py
+++ StarGo/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Celebrities)
+admin.site.register(Places)
+admin.site.register(Sightings)
+admin.site.register(Bands)
+
